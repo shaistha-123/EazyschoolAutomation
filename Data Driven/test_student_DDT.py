58427398ab6e68e81e6ffb4b7ad178082e30a511
shaistha-123 <shaistha@hyniva.com>
2019-09-30 11:46:08 +0530

    Data Driven Testing(fetching multiple data from excel sheet)

--- Data Driven/test_student_DDT.py
+++ Data Driven/test_student_DDT.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import webbrowser
-import pytest
+#import pytest
 import xlutils2
 
 class studentcase(unittest.TestCase):
@@ -20,6 +20,7 @@ class studentcase(unittest.TestCase):
     def test_methodA(self):
         baseurl = "http://dev.eazyschool.in"
         driver = webdriver.Firefox(executable_path="D:\\Pydrivers\\geckodriver.exe")
+        driver.get(baseurl)
         driver.maximize_window()
         driver.implicitly_wait(20)
         driver.get(baseurl)
@@ -29,66 +30,69 @@ class studentcase(unittest.TestCase):
         rows = xlutils2.getRowCount(path, 'Add_Student')
 
         for r in range(2,rows):
+
             username = xlutils2.readData(path, "Add_Student",r,1)
             password = xlutils2.readData(path, "Add_Student",r,2)
             StudentName = xlutils2.readData(path, "Add_Student",r,3)
             AdmissionNo = xlutils2.readData(path, "Add_Student",r,4)
             MobileNo = xlutils2.readData(path, "Add_Student",r,5)
+            Fname = xlutils2.readData(path, "Add_Student",r,6)
+            Nationality = xlutils2.readData(path, "Add_Student",r,6)
 
 
 
-        driver.find_element_by_id("j_username").send_keys("sudha@hyniva.com")
-        time.sleep(2)
-        driver.find_element_by_id("j_password").send_keys("urt123")
-        time.sleep(2)
-        driver.find_element_by_xpath("//button[@class='btn btn-success uppercase']").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//a[@id='MSTI']").click()
-        time.sleep(1)
-        driver.find_element_by_xpath("//a[@id='addStudentSetting']").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//select[@id='studyClassId']").click()
-        time.sleep(1)
-        driver.find_element_by_xpath("//option[contains(text(),'PRIMARY - A')]").click()
-        time.sleep(2)
-        # driver.find_element_by_xpath("//input[@id='as-input-032']").send_keys("PA-100")
-        driver.find_element_by_name("userVo.admissionNumber").send_keys("PA-226")
-        time.sleep(2)
-        driver.find_element_by_id("driverfName").send_keys("Sha")
-        time.sleep(1)
-        driver.find_element_by_id("categoryId").click()
-        driver.find_element_by_xpath("//option[contains(text(),'General')]").click()
-        time.sleep(1)
-        driver.find_element_by_id("fatherName").send_keys("Fname1")
-        time.sleep(2)
-        driver.find_element_by_id("stumobileNumber").send_keys("1234567890")
-        time.sleep(2)
-        driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
-                                     "/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[41]/div[1]/input[1]").click()
-        time.sleep(1)
-        driver.find_element_by_id("nationality").send_keys("Indian")
-        time.sleep(2)
-        # driver.find_element_by_id("submit_1326646977").click()
-        driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
-                                     "/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[12]/div[1]/div[1]/input[1]").click()
-        # driver.find_element_by_id("anchor_1626185855").click()
-        # driver.find_element_by_partial_link_text("javascript:void(0)").click()
-        # driver.find_element_by_class_name("fa fa-edit").click()
-        time.sleep(2)
-        # driver.find_element_by_xpath("//a[@id='urlClassAssignment']").click()
+            driver.find_element_by_id("j_username").send_keys(username)
+            time.sleep(2)
+            driver.find_element_by_id("j_password").send_keys(password)
+            time.sleep(2)
+            driver.find_element_by_xpath("//button[@class='btn btn-success uppercase']").click()
+            time.sleep(2)
+            driver.find_element_by_xpath("//a[@id='MSTI']").click()
+            time.sleep(1)
+            driver.find_element_by_xpath("//a[@id='addStudentSetting']").click()
+            time.sleep(2)
+            driver.find_element_by_xpath("//select[@id='studyClassId']").click()
+            time.sleep(1)
+            driver.find_element_by_xpath("//option[contains(text(),'PRIMARY - A')]").click()
+            time.sleep(2)
+            # driver.find_element_by_xpath("//input[@id='as-input-032']").send_keys("PA-100")
+            driver.find_element_by_name("userVo.admissionNumber").send_keys(AdmissionNo)
+            time.sleep(2)
+            driver.find_element_by_id("driverfName").send_keys(StudentName)
+            time.sleep(1)
+            driver.find_element_by_id("categoryId").click()
+            driver.find_element_by_xpath("//option[contains(text(),'General')]").click()
+            time.sleep(1)
+            driver.find_element_by_id("fatherName").send_keys(Fname)
+            time.sleep(2)
+            driver.find_element_by_id("stumobileNumber").send_keys(MobileNo)
+            time.sleep(2)
+            driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                                         "/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[41]/div[1]/input[1]").click()
+            time.sleep(1)
+            driver.find_element_by_id("nationality").send_keys(Nationality)
+            time.sleep(2)
+            # driver.find_element_by_id("submit_1326646977").click()
+            driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                                         "/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[12]/div[1]/div[1]/input[1]").click()
+            # driver.find_element_by_id("anchor_1626185855").click()
+            # driver.find_element_by_partial_link_text("javascript:void(0)").click()
+            # driver.find_element_by_class_name("fa fa-edit").click()
+            time.sleep(2)
+            # driver.find_element_by_xpath("//a[@id='urlClassAssignment']").click()
 
-        # --------Edit Student code--------------
-        """driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
-                                     "/div[1]/div[1]/div[1]/div[1]/div[6]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[10]/a[1]").click()
-        time.sleep(5)
-        driver.find_element_by_xpath("//input[@id='motherName']").send_keys("Mname")
-        time.sleep(2)
-        driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
-                                     "/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[41]/div[1]/input[1]").click()
-        # driver.find_element_by_css_selector("#submit_1080198232").click()
-        time.sleep(2)
-        driver.find_element_by_partial_link_text("Canc").click()
-        time.sleep(2)"""
+            # --------Edit Student code--------------
+            """driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                                         "/div[1]/div[1]/div[1]/div[1]/div[6]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[10]/a[1]").click()
+            time.sleep(5)
+            driver.find_element_by_xpath("//input[@id='motherName']").send_keys(Mname)
+            time.sleep(2)
+            driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[3]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]"
+                                         "/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[41]/div[1]/input[1]").click()
+            # driver.find_element_by_css_selector("#submit_1080198232").click()
+            time.sleep(2)
+            driver.find_element_by_partial_link_text("Canc").click()
+            time.sleep(2)"""
 
 if __name__=='__main__':
     unittest.main(verbosity=2)
